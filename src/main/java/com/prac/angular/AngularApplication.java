82371a34fc70df46d1a3b0c6bc0e004aad6eff58
parkjongsuk <plannist19@daum.net>
2019-01-09 18:03:31 +0900

    3

--- src/main/java/com/prac/angular/AngularApplication.java
+++ src/main/java/com/prac/angular/AngularApplication.java
@@ -2,12 +2,59 @@ package com.prac.angular;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+
 
 @SpringBootApplication
 public class AngularApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AngularApplication.class, args);
+	}
+	
+	@Configuration
+	@Order(SecurityProperties.BASIC_AUTH_ORDER)
+	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		
+		@Override
+		protected void configure(HttpSecurity http) throws Exception{
+			http
+			.httpBasic()
+			.and()
+			.authorizeRequests()
+			.antMatchers("/**") // "/index.html", "/about.html", "contact.html")
+			.permitAll()
+			.and()
+			.authorizeRequests()
+			.antMatchers("/index.html")
+			.permitAll()
+			.and()
+			.formLogin()
+			.and()
+			.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+			.and().csrf().disable();
+			//.anyRequest()
+			//.authenticated();
+		}
+		
+		@Override
+		protected void configure(AuthenticationManagerBuilder builder) throws Exception {
+			builder.inMemoryAuthentication()//.passwordEncoder(encoder)
+			.withUser("user").roles("USER").password("{noop}1234").and()
+			.withUser("admin").roles("ADMIN").password("{noop}1234");
+//			User.withDefaultPasswordEncoder().username("user").password("1234").roles("USER").build();
+//			User.withDefaultPasswordEncoder().username("admin").password("1234").roles("ADMIN").build();
+		}
 	}
 
 }
--- src/main/java/com/prac/angular/controller/AngularController.java
+++ src/main/java/com/prac/angular/controller/AngularController.java
@@ -1,14 +1,32 @@
 package com.prac.angular.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.prac.angular.model.UserVO;
 
 @Controller
 public class AngularController {
 	
 	@RequestMapping("/index")
 	public String index() {
+		System.out.println("========= index 인입 =============");
+		UserVO user = new UserVO();
 		return "index.html";
+	}
+	
+	@RequestMapping("/resource")
+	public @ResponseBody Map<String, Object> resource(){
+		System.out.println("========= resource 인입 =============");
+		Map<String, Object> map = new HashMap<>();
+		map.put("id", UUID.randomUUID());
+		map.put("content", "헬로 월드");
+		return map;
 	}
 	
 }
